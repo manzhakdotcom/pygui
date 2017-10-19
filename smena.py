b0bc3c5785c2ee62bbd344d810040e31c27b5843
Sergey Manzhak <manzhak.com@gmail.com>
2017-10-20 00:04:34 +0300

    Add class Smena()

--- smena.py
+++ smena.py
@@ -1,10 +1,20 @@
 from tkinter import *
 from tkinter import ttk
 
+class Smena:
+    def __init__(self, root):
+        self.label = ttk.Label(root, text='Hello')
+        self.label.grid(row=0, column=0, columnspan=2)
+        
+        ttk.Button(root, text='Click me!', command=self.change_label).grid(row=1, column=1)
+    
+    def change_label(self):
+        self.label.config(text='World')
 
-root = Tk()
-ttk.Button(root, text='Push me').pack()
-Label(root, text='Hello!').pack()
 
-root.mainloop()
+def main():
+    root = Tk()
+    app = Smena(root)
+    root.mainloop()
 
+if __name__ == '__main__': main()
