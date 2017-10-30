38e22f5528d06f1640d872698cd22301e46047d9
Sergey Manzhak <manzhak.com@gmail.com>
2017-10-30 18:14:13 +0300

    Add new left buttons, simple toplevel window, icon

--- smena.py
+++ smena.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -9,26 +10,27 @@ from tkinter import messagebox
 class Smena:
     def __init__(self, root):
         self.root = root
+        
         # create elements
         self.menu()
         self.frames()
-        self.label()
-        self.button()
+        self.buttonTop()
+        self.buttonLeft()
 
     def frames(self):
         self.frameTop = Frame(self.root, bd=1, relief='sunken')
-        self.frameLeft = Frame(self.root, background='#cce4ca', bd=1, relief='sunken')
-        self.frameRight = Frame(self.root, background='#f5c2c1', bd=1, relief='sunken')
+        self.frameLeft = Frame(self.root, bd=1, relief='sunken')
+        self.frameRight = Frame(self.root, bd=1, relief='sunken')
 
         self.frameTop.grid(row=0, column=0, columnspan=2, sticky='nsew', padx=2, pady=2)
         self.frameLeft.grid(row=1, column=0, sticky='nsew', padx=2, pady=2)
         self.frameRight.grid(row=1, column=1, sticky='nsew', padx=2, pady=2)
 
         self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(1, weight=10)
+        self.root.grid_rowconfigure(1, weight=12)
 
-        self.root.grid_columnconfigure(0, weight=2)
-        self.root.grid_columnconfigure(1, weight=5)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=6)
 
     def message(self):
         messagebox.showinfo(title=u'Заглушка', message=u'В разработке')
@@ -40,46 +42,73 @@ class Smena:
 
         file = Menu(menubar)
         edit = Menu(menubar)
+        tools = Menu(menubar)
         help_ = Menu(menubar)
 
         menubar.add_cascade(menu=file, label=u'Файл')
         menubar.add_cascade(menu=edit, label=u'Правка')
+        menubar.add_cascade(menu=tools, label=u'Инструменты')
         menubar.add_cascade(menu=help_, label=u'Справка')
 
-        file.add_command(label=u'Новая смена ОДЦ ЖАТС', command=self.message)
+        file.add_command(label=u'Принять смену ОДЦ ЖАТС', command=self.message)
         file.add_separator()
         file.add_command(label=u'Новая запись СЦБ', command=self.message)
         file.add_command(label=u'Новая запись АЛСН', command=self.message)
         file.add_separator()
+        file.add_command(label=u'Новый журнал замечаний СЦБ', command=self.message)
+        file.add_separator()        
+        file.add_command(label=u'Экспорт', command=self.message)        
+        file.add_separator()
+        file.add_command(label=u'Настройки', command=self.message)   
+        file.add_separator()
         file.add_command(label=u'Выйти', command=self.message)
+        
+        tools.add_command(label=u'Архив записей', command=self.message)
+        tools.add_separator()
+        tools.add_command(label=u'Статистика', command=self.message)
 
-        edit.add_command(label=u'Запись СЦБ', command=self.message)
-        edit.add_command(label=u'Запись АЛСН', command=self.message)
+        edit.add_command(label=u'Записи СЦБ', command=self.message)
+        edit.add_command(label=u'Записи АЛСН', command=self.message)
         edit.add_separator()
-        edit.add_command(label=u'Инженеры ОДЦ', command=self.message)
-        edit.add_command(label=u'Диспетчера ЦУП', command=self.message)
+        edit.add_command(label=u'Сотрудники ОДЦ ЖАТС', command=self.message)
+        edit.add_command(label=u'Сотрудники ЦУП', command=self.message)
+        edit.add_separator()
+        edit.add_command(label=u'Диспетчерские круги', command=self.message)
 
-        help_.add_command(label=u'О программе', command=self.message)
+        help_.add_command(label=u'О программе', command=self.full)
 
-    def button(self):
-        ttk.Button(self.frameTop, text=u'Запись СЦБ', width='20', command=self.message).pack(side='left', fill='y')
+    def full(self):
+        self.root.state('zoomed')
+    
+    def buttonTop(self):
+        ttk.Button(self.frameTop, text=u'+ Запись СЦБ', width='20', command=self.message).pack(side='left', fill='y', padx = 10, pady = 10)
 
-        ttk.Button(self.frameTop, text=u'Запись АЛСН', width='20', command=self.message).pack(side='left', fill='y')
+        ttk.Button(self.frameTop, text=u'+ Запись АЛСН', width='20', command= lambda : ALSN(self.root)).pack(side='left', fill='y', pady = 10)
+    
+    def buttonLeft(self):
+        ttk.Button(self.frameLeft, text=u'Записи СЦБ »', command=self.message).pack(ipady=10, padx = 10, pady = 10, fill='x')
+    
+        ttk.Button(self.frameLeft, text=u'Записи АЛСН »', command=self.message).pack(ipady=10, padx = 10,  anchor = 'nw', fill='x')        
+                
 
-
-    def label(self):
-        # self.labelTop = Label(self.frameTop, text=u'Поле вверху')
-        self.labelLeft = Label(self.frameLeft, text=u'Поле слева')
-        self.labelRight = Label(self.frameRight, text=u'Поле справа')
-        # self.labelTop.pack(side='right', fill='y')
-        self.labelLeft.pack(side='left', fill='both', expand='True')
-        self.labelRight.pack(side='bottom', fill='x')
-
+class ALSN():
+    def __init__(self, root):
+        self.root = root
+        window = Toplevel(self.root)
+        window.title(u'Добавить неисправность АЛСН')
+        window.geometry('300x200+800+400')
+        window.iconbitmap(os.getcwd() + os.path.sep +'smena.ico')
+    
+    
+class Form:
+    def __init__(self):
+        pass
 
 def main():
     root = Tk()
-    root.geometry('900x800')
+    root.geometry('900x800+500+200')
     root.title(u'Журнал замечаний СЦБ')
+    root.iconbitmap(os.getcwd() + os.path.sep +'smena.ico')
     app = Smena(root)
     root.mainloop()
 
